46da0556d5b7a5ddd01f66919cf11299b50600e3
Tearai <77899947+Tearai@users.noreply.github.com>
2023-12-06 22:50:52 +0000

    Updated the rework driving mechanic
    -reworked the driving mechanic (now the environment moves instead
    
    -the lane system still works (can be changed to wheel move instead of lane system)

--- Ordeals on the Bus/Assets/Scripts/Mian/worldmove.cs
+++ Ordeals on the Bus/Assets/Scripts/Mian/worldmove.cs
@@ -4,34 +4,89 @@ using UnityEngine;
 
 public class worldmove : MonoBehaviour
 {
+    //world move
     public float speed = 1;
-    public dockcheck dockk;
+
 
     //parking
     public float targetXPosition = 114.32f;
     public float moveSpeed = 1f;
+    public bool canPark;
+    public dockcheck dockk;
+
+    //stopping bus
+    public stopbus stoppingbus;
+
+    //going forward
+    public float gobackPosition = 109.42f;
+
+    //switching lane
+    public GameObject leftButton;
+    public GameObject rightButton;
+    public float[] lanes;
+    private int currentLaneIndex = 1;
+    private bool isSwitchingLane = false;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateBusPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dockk.dockingmode == false)
+        //Switching lane
+        if (!isSwitchingLane)
+        {
+            // Check for mouse clicks on objectToClick and objectToClick2
+            if (Input.GetMouseButtonDown(0)) // Left mouse button click
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == leftButton)
+                    {
+                        SwitchLane(-1); // Move to the left lane
+                    }
+                    else if (hit.collider.gameObject == rightButton)
+                    {
+                        SwitchLane(1); // Move to the right lane
+                    }
+                }
+            }
+        }
+
+
+
+            //Bus docking
+            if (stoppingbus.busstoping == false)
         {
             drive();
         }
-        else
+
+        if (dockk.dockingmode == true && canPark == false)
         {
             buspark();
         }
-        
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            goback();
+            canPark = true;
+            stoppingbus.busstoping = false;
+        }
+
+
     }
 
+
+    /// Bus Docking Code
     void drive()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
@@ -41,5 +96,39 @@ public class worldmove : MonoBehaviour
     {
         float step = moveSpeed * Time.deltaTime;
         transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, targetXPosition, step), transform.position.y, transform.position.z);
-    }    
+    }
+
+    void goback()
+    {
+        float step = moveSpeed * Time.deltaTime;
+        transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, gobackPosition, step), transform.position.y, transform.position.z);
+    }
+
+
+    ///Switching lane Code
+    void SwitchLane(int direction)
+    {
+        currentLaneIndex = Mathf.Clamp(currentLaneIndex + direction, 0, lanes.Length - 1);
+        StartCoroutine(MoveToLane(lanes[currentLaneIndex]));
+    }
+
+    void UpdateBusPosition()
+    {
+        StartCoroutine(MoveToLane(lanes[currentLaneIndex]));
+    }
+
+    IEnumerator MoveToLane(float targetX)
+    {
+        isSwitchingLane = true;
+
+        while (Mathf.Abs(transform.position.x - targetX) > 0.01f)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, targetX, step), transform.position.y, transform.position.z);
+            yield return null;
+        }
+
+        transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+        isSwitchingLane = false;
+    }
 }
