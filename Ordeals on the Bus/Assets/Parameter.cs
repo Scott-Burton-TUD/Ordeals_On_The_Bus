69a265b06753239606250f241cd553b3591cabab
Scott-Burton-TUD <c20341223@mytudublin.ie>
2024-01-10 22:27:37 +0000

    Final Push

---
+++ Ordeals on the Bus/Assets/Parameter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parameter : MonoBehaviour
+{
+    public string parameterName;
+    public float parameterValue;
+
+    public busSFX busSfxScript; // Reference to the busSFX script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get a reference to the busSFX script on the same GameObject
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            busSfxScript.SetBusParameter(parameterName, 0.61f);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Change the bus parameter using the reference to busSFX script
+            busSfxScript.SetBusParameter(parameterName, parameterValue);
+        }
+    }
+}
---
+++ Ordeals on the Bus/Assets/busSFX.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class busSFX : MonoBehaviour
+{
+    public EventReference bussounds;
+
+    private EventInstance bussoundsfx;
+
+
+    // Awake is called before Start
+    void Awake()
+    {
+        // Create the EventInstance for bussounds
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bussoundsfx = RuntimeManager.CreateInstance(bussounds);
+        bussoundsfx.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You can add any update logic here if needed
+    }
+
+    public void SetBusParameter(string parameterName, float parameterValue)
+    {
+        bussoundsfx.setParameterByName(parameterName, parameterValue);
+    }
+}
