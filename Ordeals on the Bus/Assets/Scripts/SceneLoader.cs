87e88e3ec5a63ce0c41e7ebeda2d4db85889d4cc
Gliafy <C20467536@mytudublin.ie>
2024-02-01 14:37:13 +0000

    Traffic crashing system

--- Ordeals on the Bus/Assets/Scripts/SceneLoader.cs
+++ Ordeals on the Bus/Assets/Scripts/SceneLoader.cs
@@ -1,35 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // The name of the scene to load.
-
+    public worldmove worldmove; // Reference to the desired worldmove script
 
     public void Start()
     {
-        GameObject someObject = GameObject.Find("WorldMap");
-        if (someObject != null)
+        if (worldmove != null)
         {
-            worldmove someScript = someObject.GetComponent<worldmove>();
+            // Call the method on the referenced script
+            worldmove.drive();
+        }
+        else
+        {
+           // Debug.LogError("worldmove script reference not set in the Inspector.");
+        }
+    }
 
-            if (someScript != null)
+    private bool isCoroutineRunning = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isCoroutineRunning)
+        {
+            Debug.Log("Player entered the trigger");
+
+            // Find the GameObject with the worldmove script using a tag
+            GameObject worldmoveObject = GameObject.FindWithTag("World");
+
+            if (worldmoveObject != null)
             {
-                someScript.drive();
+                // Get the worldmove component from the found GameObject
+                worldmove playerWorldMove = worldmoveObject.GetComponent<worldmove>();
+
+                if (playerWorldMove != null)
+                {
+                    Debug.Log("worldmove component found");
+
+                    // Save the initial speed
+                    float initialSpeed = playerWorldMove.speed;
+
+                    // Change the speed of the referenced worldmove script attached to the "Player" object.
+                    playerWorldMove.speed = 0.01f;
+
+                    // Start the coroutine only if it's not already running
+                    if (!isCoroutineRunning)
+                    {
+                        StartCoroutine(ResetSpeedAfterDelay(playerWorldMove, initialSpeed));
+                    }
+
+                    // Destroy the GameObject (assuming this script is attached to a GameObject).
+                    Destroy(gameObject, 5f);
+
+                    Debug.Log("GameObject destroyed");
+                }
+                else
+                {
+                    Debug.LogError("worldmove component not found on the GameObject with the tag 'World'.");
+                }
+            }
+            else
+            {
+                Debug.LogError("GameObject with the tag 'World' not found.");
             }
         }
+    }
 
-        void OnTriggerEnter(Collider other)
-        {
+    // Coroutine to reset speed after a delay
+    IEnumerator ResetSpeedAfterDelay(worldmove playerWorldMove, float initialSpeed)
+    {
+        isCoroutineRunning = true;
+        yield return new WaitForSeconds(2f);
 
-            if (other.CompareTag("Player"))
-            {
-                float worldmovespeed = other.GetComponent<worldmove>().speed;
-                // The object with the "Player" tag has entered the collider.
+        // Reset the speed to the initial value
+        playerWorldMove.speed = initialSpeed;
 
-            }
-        }
+        isCoroutineRunning = false;
     }
 }
