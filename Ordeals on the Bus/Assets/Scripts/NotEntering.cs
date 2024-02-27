1d9d88b9da1c67d2b3527ca985f8e4076b9be74c
Gliafy <C20467536@mytudublin.ie>
2024-02-27 13:31:04 +0000

    Karen done

---
+++ Ordeals on the Bus/Assets/Scripts/NotEntering.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotEntering : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Washer"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Washer"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
