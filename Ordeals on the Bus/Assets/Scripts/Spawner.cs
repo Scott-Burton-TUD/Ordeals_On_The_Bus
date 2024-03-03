504281ab9f27a41dc666096e9d1a02e279ff71bc
ItsSavo <77900070+ItsSavo@users.noreply.github.com>
2024-03-03 15:54:10 +0000

    T-Junction Started

--- Ordeals on the Bus/Assets/Scripts/Spawner.cs
+++ Ordeals on the Bus/Assets/Scripts/Spawner.cs
@@ -1,16 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject[] gameObjectsToActivate;
+    public GameObject objectToDeactivate;
+    public MonoBehaviour scriptToDeactivate;
+    public MonoBehaviour scriptToActivate;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            foreach (GameObject obj in gameObjectsToActivate)
+            if (objectToDeactivate != null && scriptToDeactivate != null)
             {
-                obj.SetActive(true);
+                // Deactivate the script on the specified GameObject
+                scriptToDeactivate.enabled = false;
+            }
+
+            if (scriptToActivate != null)
+            {
+                // Activate the specified script
+                scriptToActivate.enabled = true;
             }
         }
     }
