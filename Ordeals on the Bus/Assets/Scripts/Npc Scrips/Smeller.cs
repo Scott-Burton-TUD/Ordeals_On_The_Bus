89432d97ddca079783381a797a7e0ac9f5fa4c8b
Gliafy <C20467536@mytudublin.ie>
2024-03-15 18:34:30 +0000

    Mafia Added

---
+++ Ordeals on the Bus/Assets/Scripts/Npc Scrips/Smeller.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smeller : MonoBehaviour
+{
+    public GameObject particleSystemObject;
+    private ParticleSystem particleSystemComponent;
+    public bool enableParticles;
+    private Renderer objectRenderer;
+
+    void Start()
+    {
+        // Get the ParticleSystem component from the GameObject
+        particleSystemComponent = particleSystemObject.GetComponent<ParticleSystem>();
+        // Get the renderer component of the object
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        // Check if the boolean variable is true
+        if (enableParticles)
+        {
+            // Enable the particle system
+            particleSystemComponent.Play();
+        }
+        else
+        {
+            // Disable the particle system
+            particleSystemComponent.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Washer"))
+        {
+            enableParticles = false;
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else if (other.CompareTag("Bullet"))
+        {
+            // Change the color of the object to yellow
+            objectRenderer.material.color = Color.yellow;
+        }
+    }
+}
