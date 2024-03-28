998a8728d30b26737356a8199e76e68d494c5f93
Tearai <77899947+Tearai@users.noreply.github.com>
2024-03-28 15:20:14 +0000

    Stationary Object Complete

---
+++ Ordeals on the Bus/Assets/Scripts/Main/Traffic System/TrafficSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class TrafficSystem : MonoBehaviour
+{
+    public XRSlider slider;
+    public XRKnob wheel;
+
+    public float transitionspeed;
+
+    public float defaultspeed = 0.3f;
+
+    public bool crashed;
+
+    public void Update()
+    {
+        if(crashed == true)
+        {
+            wheel.value = Mathf.Lerp(wheel.value, 0.5f, transitionspeed * Time.deltaTime);
+
+            if (slider.value != defaultspeed)
+            {
+                slider.value = Mathf.Lerp(slider.value, defaultspeed, transitionspeed * Time.deltaTime);
+
+            }
+        }
+        
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Traffic"))
+        {
+
+            Vector3 newScale = other.transform.localScale;
+
+            newScale.y = 0.1f;
+
+            other.transform.localScale = newScale;
+
+            crashed = true;
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Traffic"))
+        {
+            crashed = false;
+        }
+    }
+}
